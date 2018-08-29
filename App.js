32846b0f44afc2eb21d968eef8571ebf5b7b8916
lysianne <ensoo94@gmail.com>
2018-08-29 20:55:30 +0900

    make loading, rainy state. Get geoloaction info.

--- App.js
+++ App.js
@@ -1,12 +1,30 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image, ActivityIndicator } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Image, StatusBar } from 'react-native';
+import Weather from "./Weather";
 
-export default class App extends React.Component {
+export default class App extends Component {
+  state = {
+    isLoaded: false
+  };
+  componentDidMount(){
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({
+          isLoaded: true
+        });
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
   render() {
+    const { isLoaded } = this.state;
     return (
       <View style={styles.container}>
-        <ActivityIndicator />
-        <Text>Hello World!</Text>
+        <StatusBar hidden={true} />
+        {isLoaded ? <Weather /> : <View style={styles.loading} >
+          <Text style = {styles.loadingText}>Getting the weather</Text></View>}
       </View>
     );
   }
@@ -15,8 +33,16 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff'
   },
+  loading: {
+    backgroundColor: "#FDF6AA",
+    flex: 1,
+    justifyContent: "flex-end",
+    paddingLeft: 25
+  },
+  loadingText: {
+    fontSize: 38,
+    marginBottom: 100
+  }
 });
