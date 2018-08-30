5030d9f41e8e510648dd6632b902269b67166bab
lysianne <ensoo94@gmail.com>
2018-08-30 22:40:35 +0900

    get weather from open API

--- App.js
+++ App.js
@@ -2,29 +2,50 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View, Image, StatusBar } from 'react-native';
 import Weather from "./Weather";
 
+const API_KEY = "9e6c0df4048c25a8dbc335734c058f10";
+
 export default class App extends Component {
   state = {
-    isLoaded: false
+    isLoaded: false,
+    error: null,
+    temperature: null,
+    name: null
   };
   componentDidMount(){
     navigator.geolocation.getCurrentPosition(
       position => {
-        this.setState({
-          isLoaded: true
-        });
+        this._getWeather(position.coords.latitude, position.coords.longitude);
       },
       error => {
-        console.log(error);
+        this.setState({
+          error: error
+        })
       }
     );
   }
+
+  _getWeather = (lat, lon) => {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}`)
+    .then(response => response.json())
+    .then(json => {
+      console.log(json);
+      this.setState({
+        temperature: json.main.temp,
+        name: json.weather[0].main,
+        isLoaded: true
+      })
+    })
+  }
+
   render() {
-    const { isLoaded } = this.state;
+    const { isLoaded, error } = this.state;
     return (
       <View style={styles.container}>
         <StatusBar hidden={true} />
         {isLoaded ? <Weather /> : <View style={styles.loading} >
-          <Text style = {styles.loadingText}>Getting the weather</Text></View>}
+          <Text style = {styles.loadingText}>Getting the weather</Text>
+          {error ? <Text style={styles.errorText}>{error}</Text> : null}
+          </View>}
       </View>
     );
   }
@@ -34,6 +55,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff'
+  },
+  errorText:{
+    color:"red",
+    backgroundColor:"transparent",
+    marginBottom: 40
   },
   loading: {
     backgroundColor: "#FDF6AA",
