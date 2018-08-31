49bdd6b5d95497110bf1f9d5779225a6c24bf8d8
lysianne <ensoo94@gmail.com>
2018-08-31 14:51:52 +0900

    Connect real weather info to my app and complete

--- App.js
+++ App.js
@@ -38,11 +38,11 @@ export default class App extends Component {
   }
 
   render() {
-    const { isLoaded, error } = this.state;
+    const { isLoaded, error, temperature, name } = this.state;
     return (
       <View style={styles.container}>
         <StatusBar hidden={true} />
-        {isLoaded ? <Weather /> : <View style={styles.loading} >
+        {isLoaded ? <Weather weatherName={ name } temp={Math.floor(temperature - 273.15)} /> : <View style={styles.loading} >
           <Text style = {styles.loadingText}>Getting the weather</Text>
           {error ? <Text style={styles.errorText}>{error}</Text> : null}
           </View>}
--- Weather.js
+++ Weather.js
@@ -1,26 +1,84 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View} from 'react-native';
 import { LinearGradient} from 'expo';
-import { Ionicons } from "@expo/vector-icons";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import PropTypes from "prop-types";
 
-export default class Weather extends Component{
-    render(){
-        return(
+const weatherCases={
+    Rain: {
+        colors: ["#00C6FB", "#005BEA"],
+        title: "Raining!",
+        subtitle: "Take your umbrella",
+        icon: "weather-rainy"
+    },
+    Clear: {
+        colors: ["#FEF253", "#FF7300"],
+        title: "Sunny!",
+        subtitle: "The bright days are here",
+        icon: "weather-sunny"
+    },
+    Thunderstorm:{
+        colors: ["#00ECBC", "#007ADF"],
+        title: "Thunder?",
+        subtitle: "Thun-thun-thunder, thunder, thunder",
+        icon: "weather-lightning"
+    },
+    Clouds: {
+        colors: ["#D7D2CC", "#304352"],
+        title: "Clouds,",
+        subtitle: "These clouds stick to the sky",
+        icon: "weather-cloudy"
+    },
+    Snowy: {
+        colors: ["#7DE2FC", "#B9B6E5"],
+        title: "White day!",
+        subtitle: "Cold.. You are feeling oh so cold",
+        icon: "weather-snowy"
+    },
+    Drizzle: {
+        colors: ["#89F7FE", "#66A6FF"],
+        title: "Drizzle!",
+        subtitle: "Twinkle, twinkle, little waterdrop☔️",
+        icon: "weather-hail"
+    },
+    Haze: {
+        colors: ["#bac8e0", "#6a85b6"],
+        title: "Haze!",
+        subtitle: "I'm in a blue haze",
+        icon: "weather-partlycloudy"
+    },
+    Mist: {
+        colors: ["#fbc2eb", "#a18cd1"],
+        title: "Mist!",
+        subtitle: "Just stay quiet",
+        icon: "weather-fog"
+    }
+
+}
+
+function Weather({weatherName, temp}){
+    return(
         <LinearGradient 
-            colors={["#00C6FB", "#005BEA"]} 
+            colors={weatherCases[weatherName].colors} 
             style = {styles.container}
         >
             <View style = {styles.upper}>
-                <Ionicons color="white" size={120} name="ios-rainy" />
-                <Text style ={styles.temp} >35º</Text>
+                <MaterialCommunityIcons color="white" size={120} name={weatherCases[weatherName].icon} />
+                <Text style ={styles.temp} >{temp}º</Text>
             </View>
             <View style={styles.lower}>
-                <Text style={styles.title}>Raining!</Text>
-                <Text style={styles.subtitle} >Take your umbrella.</Text>
+                <Text style={styles.title}>{weatherCases[weatherName].title}</Text>
+                <Text style={styles.subtitle} >{weatherCases[weatherName].subtitle}</Text>
             </View>
         </LinearGradient>
-        )}
+    );
 }
+
+Weather.propTypes = {
+    temp: PropTypes.number.isRequired
+}
+
+export default Weather;
 
 const styles = StyleSheet.create({
     container: {
