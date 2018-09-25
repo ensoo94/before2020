3c782ff31f9dba5352ffa791dc03f1ec9abfd473
lysianne <ensoo94@gmail.com>
2018-09-26 01:56:07 +0900

    암호문3 수정

--- 1230.cpp
+++ 1230.cpp
@@ -1,55 +1,125 @@
-#include <stdio.h>
-//list, vector로 segmentation fault --> 다 받지 말고 일부만 체크해볼 것.
-int arr[4000];
- 
-int main(void) {
-    int CN, ON, x, y, s, i, j, k, l;
-    char o[2];
-    for (i = 0; i < 10; i++) {
-        scanf("%d", &CN);
-        for (j = 0; j < CN; j++) {
-            scanf("%d", &arr[j]);
-        }
-        scanf("%d", &ON);
-        for (j = 0; j < ON; j++) {
-            scanf("%s", o);
-            if (o[0] == 'A') {
-                x = CN;
+#include <cstdio>
+#include <iostream>
+using namespace std;
+
+typedef struct list{
+	int data;
+	struct list* next;
+}node;
+
+typedef struct linkedlist{
+	struct list* head;
+}Linkedlist;
+
+void insertNode(Linkedlist* l, int data, int flag){
+	node* newNode = new node;
+	newNode->data = data;
+	newNode->next = NULL;
+	
+	if(l->head == NULL){
+        l->head = newNode;
+        return;
+    }else if(flag == 0){
+		newNode->next = l->head;
+		l->head = newNode;
+	}else if(flag == -1){
+		node* ptr = l->head;
+		while(ptr->next!=NULL) {
+                ptr = ptr->next;
             }
-            else {
-                scanf("%d", &x);
-            }
-            scanf("%d", &y);
-            if (o[0] == 'I' || o[0] == 'A') {
-                for (k = 0; k < y; k++) {
-                    scanf("%d", &s);
-                    if (x + k >= 0 && x + k <= CN && x + k < 4000) {
-                        for (l = CN - 1; l >= x + k; l--) {
-                            if (l < 3999) {
-                                arr[l + 1] = arr[l];
-                            }
-                        }
-                        arr[x + k] = s;
-                        CN++;
-                        if (CN > 4000) {
-                            CN = 4000;
-                        }
-                    }
-                }
-            }
-            else if (o[0] == 'D') {
-                if (x >= 0 && x < CN) {
-                    for (k = x; k < CN - y; k++) {
-                        arr[k] = arr[k + y];
-                    }
-                    CN -= y;
-                }
-            }
-        }
-        printf("#%d", i + 1);
-        for (j = 0; j < 10; j++) {
-            printf(" %d", arr[j]);
-        }
-        printf("\n");
-    }
+            ptr->next = newNode;
+	}else{
+		node* ptr = l->head;
+		for(int i=0;i<flag-1;i++){
+			ptr = ptr->next;
+		}
+		newNode->next = ptr->next;
+		ptr->next = newNode;
+	}
+}
+
+void addNode(Linkedlist* l, int data){
+	node* ptr = l->head;
+	
+	while(ptr->next != NULL){
+		ptr = ptr->next;
+	}
+	
+	node* newNode = new node;
+	newNode->data = data;
+	newNode->next = NULL;
+	
+	ptr->next = newNode;
+}
+
+void deleteNode(Linkedlist* l, int flag){
+	node* ptr = l->head;
+	for(int i=0;i<flag-1;i++){
+		ptr = ptr->next;
+	}
+	ptr->next = ptr->next->next;
+}
+
+int main(void)
+{
+	setbuf(stdout, NULL);
+	for(int test_case = 0; test_case  < 10; test_case++)
+	{
+		int len;
+
+		Linkedlist* l = new Linkedlist;
+		l->head = NULL;
+		
+		scanf("%d", &len);
+		
+		
+		for(int i=0;i<len;i++){
+			int tmpNum;
+			scanf("%d", &tmpNum);
+			insertNode(l, tmpNum, -1);
+		}
+		int commNum;
+		scanf("%d", &commNum);
+		
+		for(int j=0;j<commNum;j++){	
+			char tmpComm;		
+			scanf("\n %c", &tmpComm);
+			if(tmpComm == 'I'){
+				int beginNum, endNum;
+				scanf("%d %d ", &beginNum, &endNum);
+					for(int n=0;n<endNum;n++){
+						int tmpp;
+						scanf("%d ", &tmpp);
+						insertNode(l, tmpp, beginNum+n);
+					}
+			}else if(tmpComm == 'A'){
+				int endNum;
+				scanf( "%d ", &endNum);
+					for(int k=0;k<endNum;k++){
+						int tmpp;
+						scanf("%d ", &tmpp);
+						addNode(l, tmpp);
+					}
+			}else if(tmpComm == 'D'){
+				int beginNum, endNum;
+				scanf("%d %d ", &beginNum, &endNum);
+					for(int m=0;m<endNum;m++){
+						deleteNode(l, beginNum);
+					}
+			}else {
+				printf("error\n");
+			}
+		}
+		
+		
+		printf("#%d", test_case+1);
+		node* ptr = l->head;
+		for(int k=0;k<10;k++){
+			printf(" %d ", ptr->data);
+			ptr = ptr->next;
+		}
+		printf("\n");
+		delete(l);
+	}
+	return 0;
 }
