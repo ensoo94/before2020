6f0f146e11fbd4cae2619428fe68ba680181063b
JinMBP <sifnoc09@gmail.com>
2020-07-09 00:55:03 +0900

    Submit solution for '배달'

---
+++ 신진환/배달/solution.py
@@ -0,0 +1,32 @@
+def solution(N, road, K):
+    start_point = 1
+    shortest_length = { l:10000*N for l in range(1, N+1)}
+    shortest_length[start_point] = 0
+    
+    queue = [n for n in range(1, N+1)]
+    
+    while queue:
+        for r in road:
+            if r[0] == start_point:
+                shortest_length[r[1]] = min(shortest_length[start_point] + r[2], shortest_length[r[1]])
+            if r[1] == start_point:
+                shortest_length[r[0]] = min(shortest_length[start_point] + r[2], shortest_length[r[0]])
+
+        queue.remove(start_point)
+        
+        # Select Next Starting point
+        tmp = 10000*N
+        for q in queue:
+            if tmp > shortest_length[q]:
+                tmp = shortest_length[q]
+                start_point = q
+
+        if tmp == 10000*N:
+            break
+    
+    answer = 0
+    for key in shortest_length.keys():
+        if shortest_length[key] <= K:
+            answer += 1
+        
+    return answer
