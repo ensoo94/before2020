886356c43c4774f5a908146c2bccd7b0937a3d0a
Suyeong.lee <ensoo94@gmail.com>
2020-07-14 14:11:22 +0900

    배상 비용 최소화 heapq로 풀이

--- 이수영/배상 비용 최소화/solution.py
+++ 이수영/배상 비용 최소화/solution.py
@@ -1,12 +1,18 @@
+import heapq
 def solution(no, works):
     result = 0
-    #가장 큰 수 찾아서 깎기
-    while no > 0 :
-        works.sort()
-        if works[-1] <= 0 :
-            break
-        works[-1]-=1
-        no-=1
+    works = [-j for j in works] # max_heap
+    heapq.heapify(works)
     
+    # 남은 일의 합 < no면 무조건 0
+    if -sum(works) <= no :
+        return 0
+    
+    # 가장 큰 수 찾아서 깎기
+    while no > 0:
+        work = heapq.heappop(works) + 1
+        heapq.heappush(works, work)
+        no -= 1
+       
     result = sum([i**2 for i in works])
     return result
