a3080f8d9e9756c370de8e16fbb60b3c30b7c458
Suyeong Lee <ensoo94@gmail.com>
2020-07-19 16:53:22 +0900

    쇠막대기, 빙고(효율성 통과x) 풀이입니다.

---
+++ 이수영/빙고/solution.py
@@ -0,0 +1,32 @@
+from collections import Counter
+
+def solution(board, nums):
+    answer = 0
+    d_arr1 = []
+    d_arr2 = []
+    
+    # 숫자 지우기, 대각행렬 구하기
+    for i in range(0, len(board)):
+        for j in range(0, len(board)):
+            if i == j:
+                d_arr1.append(board[i][j])
+            if j == len(board)-i-1:
+                d_arr2.append(board[i][j])
+            if board[i][j] in nums:
+                board[i][j] = 0
+    
+    # 빙고 세기
+    for r_line in board: #row
+        if r_line == [0]*len(board):
+            answer += 1
+    for c_line in list(zip(*board)): #col
+        if list(c_line) == [0]*len(board):
+            answer += 1
+            
+    # 대각선
+    if len(Counter(nums) - Counter(d_arr1)) == len(nums) - len(board):
+        answer += 1
+    if len(Counter(nums) - Counter(d_arr2)) == len(nums) - len(board):
+        answer += 1
+
+    return answer
