428b38889a72acf59c53143a8e8a44ada3f6d78d
Suyeong Lee <ensoo94@gmail.com>
2020-07-19 17:35:25 +0900

    나머지 한 점, 운송 트럭 풀이입니다.

---
+++ 이수영/운송 트럭/solution.py
@@ -0,0 +1,18 @@
+from math import ceil
+
+def solution(max_weight, specs, names):
+    answer = 1
+    remain_weight = max_weight
+    
+    # dictionary 이용
+    specs = dict(specs)
+    
+    for product in names:
+        weight = int(specs[product])
+        if weight > remain_weight:
+            answer += 1
+            remain_weight = max_weight - weight
+        else:
+            remain_weight -= weight
+    
+    return answer
