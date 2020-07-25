4060ef26281f4995fd846995acffd78570e9380c
Suyeong Lee <ensoo94@gmail.com>
2020-07-26 00:25:05 +0900

    필요없는 파일 삭제

--- 신진환/완주하지 못한 선수/solution.py
+++
@@ -1,9 +0,0 @@
-def solution(participant, completion):
-    participant_dict = {p:0 for p in participant}
-    
-    for p, c in zip(participant, completion):
-        participant_dict[p] += 1
-        if c:
-            participant_dict[c] -= 1
-        
-    return max(participant_dict, key=participant_dict.get)
